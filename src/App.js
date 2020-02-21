bc4137e1c39c8c2e34608ea86bcf7b919f1d4935
Carlos Alberto Albistur Urrutia <carlos.albistur@tanner.cl>
2020-02-21 15:12:59 -0300

    commit, 'tasksapp en vias de desarrollo'

--- src/App.js
+++ src/App.js
@@ -1,66 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-/*
-function Helloworld(props){
-  console.log(props) //props: datos que un componente recibe a traves de un objeto (props, propiedades)
-  return(
-    <div id="hello"> 
-    <h3>{props.subtitle}</h3>
-    {props.mytext} 
+import tasks from './sample/tasks.json';
+
+import Tasks from './components/Tasks';
+
+//console.log(tasks)
+class App extends Component {
+state = {
+  tasks: tasks
+}
+
+  render() {
+    return <div>
+      {this.state.tasks.map(e => <p key={e.id}>
+        {e.title} - {e.description} - {e.done} - {e.id}
+        <Tasks/>
+      </p>)}
     </div>
-  )
-}
-*/
-
-class Helloworld extends React.Component {
-
-  state ={
-    show: true
   }
 
-toggleShow = () => {
-  this.setState({show: !this.state.show}) 
-  /*console.log(this.state.show)
-    if(this.state.show){
-    this.setState({show: false}) 
-  }
-  else
-    this.setState({show: true})*/    
+
+
+
 }
 
-  render(){
-    if (this.state.show){
-      return(
-        <div id="hello"> 
-        <h3>{this.props.subtitle}</h3>
-        {this.props.mytext} 
-        <button onClick={ this.toggleShow }> Cambiar Estado</button>
-        </div>
-      )
-    }
-    else{
-    return <h1>"No hay elementos" <button onClick = {this.toggleShow}> Cambiar Estado</button></h1>
-    }
-  }
-}
-
-/*
-const App = () => <div>This is my component: <Helloworld/> <Helloworld/> <Helloworld/> </div>
-
-class App extends React.Component{
-  render(){
-    return <div>This is my component: <Helloworld/> <Helloworld/> <Helloworld/></div>
-  }
-}
-*/
-function App() {
-  return (
-    <div>This is my component: 
-      <Helloworld mytext = "Hello Fazt" subtitle = "Demsey Roll"/> 
-      <Helloworld mytext = "Hola Mundo" subtitle = "ultra"/> 
-      <Helloworld mytext = "Hola" subtitle = "componente 3"/></div>
-  );
-}
 
 export default App;
